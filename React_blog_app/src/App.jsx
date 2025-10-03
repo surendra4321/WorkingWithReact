4ef6e745ebd38ce6c444fd001d48bcd4fafd37f9
suren <surendskyrk870@gmail.com>
2025-10-03 10:10:12 +0530

    Com between two component using the probs

--- React_blog_app/src/App.jsx
+++ React_blog_app/src/App.jsx
@@ -7,57 +7,58 @@ import Login from './UserComponent'
 import Sates from './States'
 import Count from './Count'
 import { Condition } from './Condition'
+import User from './User'
 
 function App() { 
-alert(sum())
+//alert(sum())
   return ( 
     <div> 
-       <Header/>
-       <h1>Hello!! This is my first react application</h1>
-       <h2>Welcome to react</h2>  
-       <Fruits />
+       {/* <Header/> */}
+       <h1>Probs in React JS</h1>
+       {/* <Fruits />
        <Colour />
        <Login />
        <TodoList/>
        <ExapComp/> 
        <Sates />
        <Count/>
-       <Condition/>
+       <Condition/>  */}
+       <User name="Nischal"/>
        </div>     
   )
 }
-function Fruits(){
-  return(
-    <h1>Apple</h1>
-  )
-}
-function Colour(){
-  return(
-    <h1>Red Color</h1>
-  )
-}
+// function Fruits(){
+//   return(
+//     <h1>Apple</h1>
+//   )
+// }
+// function Colour(){
+//   return(
+//     <h1>Red Color</h1>
+//   )
+// }
 
-function sum(){
-  return 10+20
-}
-  function TodoList() {
-  return (
-      // This doesn't quite work!
-      <>
-    <h1>Hedy Lamarr's Todos</h1>
-    <img 
-      src="https://i.imgur.com/yXOvdOSs.jpg" 
-      alt="Hedy Lamarr" 
-      class="photo"
-    />
-    <ul>
-      <li>Invent new traffic lights</li>
-      <li>Rehearse a movie scene</li>
-      <li>Improve the spectrum technology</li>
-    </ul>
-    </>
-  );
-}
+// function sum(){
+//   return 10+20
+// }
+//   function TodoList() {
+//   return (
+//       // This doesn't quite work!
+//       <>
+//     <h1>Hedy Lamarr's Todos</h1>
+//     <img 
+//       src="https://i.imgur.com/yXOvdOSs.jpg" 
+//       alt="Hedy Lamarr" 
+//       class="photo"
+//     />
+//     <ul>
+//       <li>Invent new traffic lights</li>
+//       <li>Rehearse a movie scene</li>
+//       <li>Improve the spectrum technology</li>
+//     </ul>
+//     </>
+//   );
+// }
 
 
 export default App
---
+++ React_blog_app/src/User.jsx
@@ -0,0 +1,9 @@
+function User({name}){
+    //console.log(probs);
+    return(
+       <>
+       <h1>User :{name}</h1>     
+       </>
+    );
+}
+export default User;
